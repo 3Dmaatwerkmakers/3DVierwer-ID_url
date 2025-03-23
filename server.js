0509193b09d84ee82435a53b0e1d69daca1bfd03
3Dmaatwerkmakers <3Dmaatwerkmakers@gmail.com>
2025-03-23 16:48:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const cors = require('cors');
-app.use(cors());  // Dit staat alle bronnen toe om toegang te krijgen tot je API
-
+const cors = require('cors');  // Zorg ervoor dat je cors importeert na express
 const { google } = require('googleapis');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Voeg de root route toe voor de homepage
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
+// Zorg ervoor dat CORS voor de app wordt ingesteld vóór de routes
+app.use(cors());  // Dit staat alle bronnen toe om toegang te krijgen tot je API
 
 // Maak de Google Drive API client
 async function getFile(fileId) {
   const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_SERVICE_ACCOUNT_KEY,// Voeg hier je Google Service Account Key toe
+    keyFile: 'path-to-your-service-account-key.json', // Voeg hier je Google Service Account Key toe
     scopes: ['https://www.googleapis.com/auth/drive.readonly'],
   });
   const client = await auth.getClient();
