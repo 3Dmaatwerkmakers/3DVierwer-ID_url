e211fdb5f74f30cf02b98a953be247d4802f53ce
3Dmaatwerkmakers <3Dmaatwerkmakers@gmail.com>
2025-03-23 15:33:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Stel een eenvoudige route in
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js server!');
+});
+
+// Start de server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
